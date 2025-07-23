724f8990d85c417e6d21ccfe1405f31dd8891c3b
Manaswini-118 <manaswinibatthula@gmail.com>
2025-07-23 11:39:35 +0530

    Add files via upload
    OOPS SKILL week -1

---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+package programs;
+
+public class TypeConversion {
+	public static void main(String[] args) {
+		int n = 100;
+		double d = n; // int to double (implict conversion)
+		System.out.println("Integer value: " +n);
+		System.out.println("Converted to double: " +d);
+	}
+}
---
+++ whileExample.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class whileExample {
+	public static void main(String args[])
+	{
+	int x=2;
+	while(x<=5)
+	{
+	System.out.println("hello");
+	x++;
+	}}
+	}
+
