7891cc2b712b1f2fb64566a15589363e0ef61b1a
Manaswini-118 <manaswinibatthula@gmail.com>
2025-07-26 09:19:21 +0530

    Add files via upload
    practical week 1

---
+++ Arthmeticoperators.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class Arthmeticoperators {
+	public static void main(String[] args) {
+		int a = 5, b = 7;
+		System.out.println("Addition: " + (a+b));
+		System.out.println("Subraction: " + (a-b));
+		System.out.println("Multiplication: " + (a*b));
+		System.out.println("Division: " + (a/b));
+		System.out.println("Modulus: " + (a%b));
+	}
+}
+
