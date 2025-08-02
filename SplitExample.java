d9cd42abd82edcae2d41e2b123837cc8f728c8fe
Manaswini-118 <manaswinibatthula@gmail.com>
2025-08-02 09:18:11 +0530

    Add files via upload

---
+++ SplitExample.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class SplitExample {
+	 public static void main(String[] args) {
+	        String s = "apple,banana,grape,orange";
+	        String[] fruits = s.split(",");
+	        for(String fruit : fruits) {
+	            System.out.println(fruit);
+	        }
+	    }
+
+
+}
