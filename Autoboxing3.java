b5e3b23ab2373d827b7f47709f8523d2eaf88265
Manaswini-118 <manaswinibatthula@gmail.com>
2025-08-02 09:28:28 +0530

    Add files via upload

---
+++ Autoboxing3.java
@@ -0,0 +1,11 @@
+package programs;
+import java.util.ArrayList;
+class Autoboxing3 {
+	public static void main(String[] args) {
+		int x=10;
+		Integer obj=x;
+		System.out.println("primitivevalue of integer is " + x);
+		System.out.println("autoboxing obj is" + obj);
+	}
+
+}
